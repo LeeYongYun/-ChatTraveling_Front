05468dfdb0b9451a64acd1dd88b2997111e204d8
LeeYongYun <yj9334@naver.com>
2023-08-09 09:41:05 +0900

    첫 번째 커밋

---
+++ src/component/chatui.tsx
@@ -0,0 +1,28 @@
+import chatCore from "./chat-core";
+import type{responseProps} from "./chat-core";
+
+interface footerData {
+  data:string,
+  footerdata:string
+}
+interface responseChat extends responseProps{
+  data? : chatData[]
+}
+interface chatData {
+  sendChat: string,
+  receiveChat : string,
+}
+
+const chatui = ({data}:{data:footerData}) =>{
+  chatCore.postAxios(process.env.NEXT_PUBLIC_API_URL + "/insert",{},(response:responseChat)=>{
+      if(response.data && response.data.length > 1){
+        alert("데이터 호출에 성공하였습니다.");
+      }
+    },(error:responseChat)=>{
+      alert(error.errMsg);
+    })
+
+    return (<div><h1>{data.data}</h1>{data.footerdata}{process.env.NEXT_PUBLIC_API_URL}</div>);
+} 
+
+export default chatui;
